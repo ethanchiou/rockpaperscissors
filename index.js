c528a5a5e13de489fa5b2a2eb97aa76e10653f27
ethanchiou <147359245+ethanchiou@users.noreply.github.com>
2025-05-13 16:52:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const choices = ["rock", "paper", "scissor"];
+const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore=0;
+let computerScore=0;
 
 function playGame(playerChoice){
     //Computer choice is random index from 0-2, for rock/paper/scissor respectively
@@ -34,9 +39,13 @@ function playGame(playerChoice){
     switch(result){
         case "YOU WIN!":
             resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
             break;
         case "YOU LOSE!":
             resultDisplay.classList.add("redText");
+            computerScore++
+            computerScoreDisplay.textContent = computerScore;
             break;
         case "IT'S A TIE":
             resultDisplay.classList.add("blueText");
