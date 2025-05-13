610b288bdf41cd5dd14a4f282aa619393e0d678d
ethanchiou <147359245+ethanchiou@users.noreply.github.com>
2025-05-13 16:34:06 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const choices = ["rock", "paper", "scissor"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+    //Computer choice is random index from 0-2, for rock/paper/scissor respectively
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+    let result=""
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                //return win/loss based on tenariy operator
+                result=(computerChoice === "scissors")?"YOU WIN!":"YOU LOSE!";
+                break;
+            case "paper":
+                result=(computerChoice === "rock")?"YOU WIN!":"YOU LOSE!";
+                break;
+            case "scissors":
+                result=(computerChoice === "paper")?"YOU WIN!":"YOU LOSE!";
+                break;
+        }
+    }
+    playerDisplay.textContent=`PLAYER: ${playerChoice}`;
+    computerDisplay.textContent=`COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent=result;
+
+    //remove and colorText class before case to reset the colors(prevents past iterations from affecting color)
+    resultDisplay.classList.remove("greenText","redText","blueText");
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            break;
+        case "IT'S A TIE":
+            resultDisplay.classList.add("blueText");
+            break
+    }
+
+}
